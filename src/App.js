d68efc79b4e548482e8f9541520f8e062f32efea
Vatsal <vatsalpipalava@gmail.com>
2023-09-24 19:26:54 +0530

    update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navigationbar from './components/navbar';
 
 function App() {
-  const apiUrl = 'https://inshorts.me/news/all?offset=0&limit=99';
   return (
     <>
       <Navigationbar />
